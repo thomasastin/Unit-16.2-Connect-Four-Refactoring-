e8543e66500898b4335c41e9cd98b2e8a8e9e1cc
thomasastin <163066303+thomasastin@users.noreply.github.com>
2024-09-09 18:46:52 -0400

    Add files via upload

---
+++ Unit 16.2 Connect Four Refactoring Javascript part of assignment by Thomas Astin.js
@@ -0,0 +1,134 @@
+class Game {
+    constructor(height = 6, width = 7) {
+        this.height = height;
+        this.width = width;
+        this.board =[];
+        this.currPlayer = new this.currPlayer('red');
+        this.players = [new this.players('red'), new Player('blue')];
+        this.gameOver = false;
+        this.makeBoard();
+        this.makeHtmlBoard();
+    }
+
+}
+class Player {
+    constructor(color) {
+        this.color = color;
+    }
+
+    makeBoard() {
+        for (let y = 0; y < this.height; y++ {
+        this.board.push(Array.from({ length: this.width }));
+    
+    }
+}
+
+makeHtmlBoard() {
+    const board = document.getElementById('board');
+    board.innerHTML =  '';
+    
+    const top = document.createElement('tr');
+    top.setAttribute('id', 'column-top');
+    top.addEventListener('click', this.handleClick.bind(this));
+
+    for (let x = 0; x < this.width; x++) {
+        const headCell = document.createElement('td');
+        headCell.setAttribute('id', x);
+        top.append(headCell);
+    }
+
+    board.append(top);
+
+    for(let y = 0; y < this.height; y++) {
+        const row = document.createElement('tr');
+
+
+        for(let x = 0; x< this.width; x++) {
+            const cell = document.createElement('td');
+            cell.setAttribute('id', `${y}-${x}`);
+            row.append(cell);
+        }
+
+        board.append(row);
+    }
+}
+
+findSpotForCol(x) {
+    for (let y = this.height -1; y>= 0; y--) {
+        if (!this.board[y][x]) {
+            return y;
+        }
+    }
+    return null;
+}
+
+placeInTable(y, x) {
+    const piece = document.createElement('div');
+    piece.classList.add('piece');
+    piece.style.backgroundColor = this.currPlayer.color;
+    piece.style.top = -50 * (y + 2);
+
+    const spot = document.getElementById(`${y}-${x}`);
+    spot.append(piece);
+}
+
+endGame(msg) {
+  alert(msg);
+  this.gameOver = true;
+
+}
+
+handleClick(evt) {
+    if (this.gameOver) return;
+    const x = +evt.target.id;
+    const y = this.findSpotForCol(x);
+    if (y === null) {
+        return;
+    }
+
+
+    this.board[y][x] = this.currPlayer;
+    this.placeInTable(y, x);
+
+    if(this.checkForWin()) {
+        return this.endGame(`Player  $(this.currPlayer.color} won!`);
+
+    if(this.board.every(row => row.every(cell => cell))) {
+            return this.endGame(`Tie!`);
+        }
+
+    this.currPlayer = this.currPlayer === this.players[0] ? this.players[1] :
+this.players[0];
+    }
+
+    checkForWin() {
+        const_win = (cells) => {
+            return cells.every(
+                [y, x]) =>
+                y >= 0&&
+                y< this.height &&
+                x >= 0 &&
+                x < this.width &&
+                this.board[y][x] === this.currPlayer
+
+            );
+        };
+
+        for (let y = 0;y < this.height; y++) {
+            for(let x = 0; x < this.width; x++) {
+                const horiz - [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
+                const vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
+                const diagDR = [[y, x], [y + 1, x + 1]], [y + 2, x + 2], [y + 3, x + 3]];
+                const diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
+
+                if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
+                    return true;
+                }
+            }
+        }
+    }
+}
+
+document.getElementById('start-game').addEventListener('click', () => {
+    new Game(6, 7);
+});
